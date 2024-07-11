98f38492d7ee066d11ff358d244626f0b997d975
Sumerk-2024 <163854268+Sumerk-2024@users.noreply.github.com>
2024-07-11 18:03:01 +0300

    Add folder Dz-1
    Добавить папку Dz-1.

---
+++ Dz-1/test.py
@@ -0,0 +1,32 @@
+import unittest
+from main import add, subtract, multiply, divide, mod
+
+
+class TestMath(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 5), 7)
+        self.assertNotEqual(add(3, 7), 9)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(7, 4), 3)
+        self.assertNotEqual(subtract(4, 2), 1)
+
+    def test_multiply(self):
+        self.assertNotEqual(multiply(2, 5), 12)
+        self.assertEqual(multiply(3, 6), 18)
+
+    def test_divide(self):
+        self.assertNotEqual(divide(4, 2), 3)
+        self.assertEqual(divide(20, 5), 4)
+        with self.assertRaises(ValueError):
+            divide(10, 0)
+
+    def test_mod(self):
+        self.assertEqual(mod(10, 3), 1)
+        self.assertEqual(mod(20, 5), 0)
+        with self.assertRaises(ValueError):
+            mod(10, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
